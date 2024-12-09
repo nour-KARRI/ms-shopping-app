f9e6aa4ea853e8c7c654c8847fedf5e72dcdd4f9
NOUREDDINE KARRI <noureddine.karri@atos.net>
2024-12-09 14:24:11 +0100

    Add kafka to order-service

--- order-service/src/main/java/com/nour/ms/order/service/OrderService.java
+++ order-service/src/main/java/com/nour/ms/order/service/OrderService.java
@@ -2,12 +2,14 @@ package com.nour.ms.order.service;
 
 import com.nour.ms.order.client.InventoryClient;
 import com.nour.ms.order.dto.OrderRequest;
+import com.nour.ms.order.event.OrderPlacedEvent;
 import com.nour.ms.order.model.Order;
 import com.nour.ms.order.repository.OrderRepository;
 import groovy.util.logging.Slf4j;
 import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
 import java.util.UUID;
@@ -19,6 +21,7 @@ public class OrderService {
 
 	private final OrderRepository orderRepository;
 	private final InventoryClient inventoryClient;
+	private final KafkaTemplate<String, OrderPlacedEvent> kafkaTemplate;
 	public String placeOrder(OrderRequest orderRequest){
 
 		var isProductInStock = inventoryClient.isInStock(orderRequest.skuCode(), orderRequest.quantity());
@@ -30,9 +33,20 @@ public class OrderService {
 					.quantity(orderRequest.quantity())
 					.build();
 			orderRepository.save(order);
+
+			OrderPlacedEvent orderPlacedEvent = new OrderPlacedEvent(order.getOrderNumber(), orderRequest.userDetails().email());
+
+			/** send a message to kafkaTopic:
+			 	- key: order-placed
+			 	- value: OrderNumber, email
+			 */
+			log.info("Start - Sending OrderPlaceEvent {} to Kafka topic order-placed", orderPlacedEvent);
+			kafkaTemplate.send("orderPlaced", orderPlacedEvent);
+			log.info("End - Sending OrderPlacedEvent {} to kafka topic order-placed", orderPlacedEvent);
+
 			return "Order placed Successfully";
 		}else{
-			log.error("Product with SkuCode " + orderRequest.skuCode() + " is not in stock");
+			log.error("Product with SkuCode {} is not in stock", orderRequest.skuCode());
 			return "Ordering failed";
 		}
 	}
